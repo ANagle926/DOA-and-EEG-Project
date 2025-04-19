6560fc08d4dfcb1ef21230eb506cb5390c006818
ANagle926 <nikunagle@gmail.com>
2025-04-19 18:30:33 -0400

    Finished Intraoperative Regression!!

--- Intraoperative Models/RegressionTest.py
+++ Intraoperative Models/RegressionTest.py
@@ -16,7 +16,7 @@ seglen = dataset.SEGLEN
 # Initialize EEGRegressor object
 eeg_regressor = EEGRegressor(x_train, y_train, x_test, y_test, c_test, seglen)
 
-# Perform hyperparameter tuning
+""""# Perform hyperparameter tuning
 best_params= eeg_regressor.hyperparameter_tuning()
 model= eeg_regressor.model
 dump(model, "/mnt/c/Users/Nagle2/PycharmProjects/DOA-and-EEG-Project/Data Files/Model Versions/eeg_regressor.joblib")
@@ -34,6 +34,7 @@ print("best dropout:", best_dropout)
 print("best reg strength:", best_reg_strength)
 print("best learning rate:", best_learning_rate)
 print("best epochs:", best_epochs)
-print("best batch size:", best_batch_size)
+print("best batch size:", best_batch_size)"""
 
+eeg_regressor.train_model()
 eeg_regressor.evaluate_model()
